fc6f7f83d477454d831d2adc68a705fc5adddb3a
Rubasri S <rubasrisrikanthan@gmail.com>
2023-08-24 20:31:12 +0530

    Deploy website

---
+++ build/precache-manifest.84df28f67fd984e0d60ad5c21b24a42f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d58d9669f240a6674a494c579277e9",
+    "url": "/keeper-app-part3/index.html"
+  },
+  {
+    "revision": "aed5558a41a5c248a185",
+    "url": "/keeper-app-part3/static/js/2.1ec8088d.chunk.js"
+  },
+  {
+    "revision": "7444191249c69b9366c0",
+    "url": "/keeper-app-part3/static/js/main.eb5743a7.chunk.js"
+  },
+  {
+    "revision": "ddc6b733c6db0cc1f72a",
+    "url": "/keeper-app-part3/static/js/runtime-main.dedd0144.js"
+  }
+]);
